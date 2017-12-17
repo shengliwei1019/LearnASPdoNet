51a060f3ea0ef8b854bff544f53083241e3b52e4
shengliwei1019 <shengliwei1019@gmail.com>
2017-12-17 20:17:25 +0800

    UriDemo Commit

---
+++ chapter_1/1.1.1/UriDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UriDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Uri ms = new Uri("http://www.microsoft.com/en/us/default.aspx");
+            Console.WriteLine("Scheme: {0}", ms.Scheme);
+            Console.WriteLine("Host: {0}", ms.Host);
+            Console.WriteLine("Port: {0}", ms.Port);
+            Console.WriteLine("AbsolutePath: {0}", ms.AbsolutePath);
+            Console.WriteLine("Query: {0}", ms.Query);
+            Console.ReadKey();
+        }
+    }
+}
