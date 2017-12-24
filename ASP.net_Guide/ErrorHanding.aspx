fcfaead19a777e2c3c19bbf6c553f5786fd2e94b
shengliwei1019 <shengliwei1019@gmail.com>
2017-12-24 21:27:55 +0800

    ASP.net Guide Init Commit

---
+++ ASP.net_Guide/ErrorHanding.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ErrorHanding.aspx.cs" Inherits="ASP.net_Guide.ErrorHanding" Trace="true" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>Tracing, debugging and error handing</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Label ID="lblheading" runat="server" Text="Tracing, Debugging and Error Handing"></asp:Label>
+        <br /><br />
+        <asp:DropDownList ID="ddlquotes" runat="server" AutoPostBack="true" OnSelectedIndexChanged="ddlquotes_SelectedIndexChanged"></asp:DropDownList>
+        <br /><br />
+        <asp:Label ID="lblquotes" runat="server"></asp:Label>
+        <br /><br />
+        <asp:HyperLink ID="HyperLink1" runat="server" NavigateUrl="file:///C:/Program%20Files/Git/mingw64/share/doc/git-doc/git-status.html">Link to:</asp:HyperLink>
+    </div>
+    </form>
+</body>
+</html>
---
+++ ASP.net_Guide/ErrorHanding.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ASP.net_Guide
+{
+    public partial class ErrorHanding : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Trace.Write("Page Load");
+
+            if (!IsPostBack)
+            {
+                Trace.Write("Not Post Back, Page Load");
+                string[,] quotes =
+                {
+                    {"Imagination is more important than Knowledge.", "Albert Einsten" },
+                    {"Assume a virtue, if you have it not", "Shakespeare" },
+                    {"A man cannot be comfortable without his own approval", "Mark Twain" }
+                };
+
+                for (int i = 0; i < quotes.GetLength(0); i++)
+                {
+                    ddlquotes.Items.Add(new ListItem(quotes[i, 0], quotes[i, 1]));
+                }
+            }
+        }
+
+        protected void ddlquotes_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (ddlquotes.SelectedIndex != -1)
+            {
+                lblquotes.Text = string.Format("{0}, Quote: {1}", ddlquotes.SelectedItem.Text, ddlquotes.SelectedValue);
+            }
+
+            try
+            {
+                int a = 0;
+                int b = 9 / a;
+            }
+            catch (Exception ex)
+            {
+                Trace.Warn("UserAction", "processing 9/a", ex);
+            }
+        }
+    }
+}
